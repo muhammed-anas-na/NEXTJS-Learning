a8bcc0bd7d51770b752fec2ce4bfc5baa3ecba23
Anas <anasna6005@gmail.com>
2023-12-28 21:03:30 +0530

    Parallel routing implemented

---
+++ src/app/complex-dashboard/layout.tsx
@@ -0,0 +1,24 @@
+export default function CompleDashboardLayout({
+    children,
+    users,
+    revenue,
+    notification
+} : {
+    children: React.ReactNode;
+    users: React.ReactNode;
+    revenue: React.ReactNode;
+    notification: React.ReactNode;
+}){
+    return (
+        <div>
+            <div>{children}</div>
+            <div style={{display:"flex"}}>
+                <div style={{display:"flex" , flexDirection:"column"}}>
+                    <div>{users}</div>
+                    <div>{revenue}</div>
+                </div>
+                <div style={{display:"flex" , flex:1}}>{notification}</div>
+            </div>
+        </div>
+    )
+}
